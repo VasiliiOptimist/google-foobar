400c36fc63516d5d762e318677dedfff0eb1c3dc
Vasya <vasya_mk@mail.ru>
2023-06-07 22:22:24 +0300

    Update message.py

--- message.py
+++ message.py
@@ -1,9 +1,1 @@
-import base64
-
-encrypted = 'HlUeFAgME10FUkVITUYMHRNPAlJJUkoCBAMaSxcSEBdKQVFPUUsFAQAXAAQPSFoOURADFAITHxxR DkxVQhsDAhkKEkcUGQBVQUFMDhVGHxATFwAEBRtRDkxVQgcDDQQMHUsSUklSShMKDRRHAgZCUldB TBwXSBNSSVJKBwQAUQ5MVUIFBA9KSAs='
-my_eyes=str.encode("ermakov.vu")
-decoded=base64.b64decode(encrypted)
-decrypted=""
-for i in range(0,len(decoded)):
-    decrypted+=chr((my_eyes[i%len(my_eyes)] ^ decoded[i]))
-print(decrypted)
+{'success' : 'great', 'colleague' : 'esteemed', 'efforts' : 'incredible', 'achievement' : 'unlocked', 'rabbits' : 'safe', 'foo' : 'win!'}
